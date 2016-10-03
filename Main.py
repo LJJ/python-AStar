2cb3332c590a9b9db0ac9bf4fba3c4b7dd8adaeb
lujiji <lujiji.private@gmail.com>
2016-10-02 20:04:13 -0400

    fix read empty map error

--- Main.py
+++ Main.py
@@ -1,30 +1,28 @@
 __author__ = 'lujiji and SiyuChen'
-#import Map
-from Map import *
+import Map
 from Astar import *
-
 import random
 
 
-readMap()
+mapData = Map.readMap()
 # createMap()
-start_x, start_y, goal_x, goal_y = CreateStartGoal()
+start_x, start_y, goal_x, goal_y = Map.CreateStartGoal()
+
 
 print mapData[start_y][start_x], start_x, start_y
 print mapData[goal_y][goal_x], goal_x, goal_y
 sStart = (start_x, start_y)
 sGoal = (goal_x, goal_y)
 
-
 path_id, cost = Astar(sStart, sGoal, mapData)
 path_id.append(sStart)
 path_id.reverse()
 print path_id
-savePath(path_id, cost)
+Map.savePath(path_id, cost)
 
 for i in range(0, len(path_id)-1):
-    DrawLines(path_id[i], path_id[i+1])
-
+    Map.DrawLines(path_id[i], path_id[i+1])
+Map.mainloop()
 
 
 x = 0
