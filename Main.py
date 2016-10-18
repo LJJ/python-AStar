2f82ede8263b4ba528725010bdc800fe2ac07cfb
lujiji <lujiji.private@gmail.com>
2016-10-17 22:44:51 -0400

    read start and goal

--- Main.py
+++ Main.py
@@ -3,11 +3,18 @@ import Map
 from Astar import *
 import time
 
-def show(wa, numSG):
+def show(wa, numSG, read = False):
     # start = time.clock()
     totalCost = 0
     for j in range(0,numSG):
-        startLoc, goalLoc = map.CreateStartGoal() #Map.Location(140,104), Map.Location(1,10)
+        startLoc = None
+        goalLoc = None
+        if read is True:
+            startLoc = map.start
+            goalLoc = map.goal
+        else:
+            startLoc, goalLoc = map.CreateStartGoal()
+        # startLoc, goalLoc = map.CreateStartGoal() #Map.Location(140,104), Map.Location(1,10)
         print "S-G",j+1
         print "Start point is", mapData[startLoc.y][startLoc.x], startLoc.x, startLoc.y
         print "Goal point is", mapData[goalLoc.y][goalLoc.x], goalLoc.x, goalLoc.y
@@ -30,6 +37,7 @@ path_id = None
 cost = 0.0
 mapData = None
 x = 0
+read = False
 while x != 7:
     display = ["Please input your choice:",
                "1. Create new map",
@@ -45,14 +53,17 @@ while x != 7:
     if x == 1:
         map = Map.Map()
         mapData = map.createMap()
+        read = False
     elif x == 2:
         map = Map.Map()
         mapData = map.readMap()
+        read = True
     elif x == 3:
         start = time.clock()
         if map is not None:
             wa = input("input WA:")
-            show(wa,1)
+            show(wa, 1, read)
+
             end = time.clock()
             print 'Running time is:', end-start
             Map.mainloop()
